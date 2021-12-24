dffa78f64515d9301efbf0229e13ae79f6939933
hmtmcse <hmtm.cse@gmail.com>
2021-12-24 20:48:14 +0600

    Flask SQLAlchemy A Minimal Application

---
+++ minimal_application.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask-sqlalchemy.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def bismillah():
+    return "Flask SQLAlchemy Tutorial"
+
+
+if __name__ == '__main__':
+    app.run()
